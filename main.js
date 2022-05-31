ce888f5f62eb6c9b5d07284f5bd5204a94e1fb15
Dor <dorzairi@ymail.com>
2022-05-30 21:46:50 -0400

    earesing completed

--- main.js
+++ main.js
@@ -1,85 +1,54 @@
-var config = {
-  apiKey: "AIzaSyDJFBVLuUH5ye3BS05-V254VzoBwoN_nqs",
-  authDomain: "cruddemo-25369.firebaseapp.com",
-  databaseURL: "https://cruddemo-25369-default-rtdb.firebaseio.com",
-  projectId: "cruddemo-25369",
-  storageBucket: "cruddemo-25369.appspot.com",
-  messagingSenderId: "887636253677",
-  appId: "1:887636253677:web:020109f04deb8f835aa5e4"
+/**
+ * Config File
+ */
 
-};
+// TODO 2, Initialize App
 
-firebase.initializeApp(config);
-var db = firebase.database();
 
-// CREATE REWIEW
+//  TODO 4, Add handlers
 
 var reviewForm = document.getElementById('reviewForm');
 var fullName   = document.getElementById('fullName');
 var message    = document.getElementById('message');
 var hiddenId   = document.getElementById('hiddenId');
 
-reviewForm.addEventListener('submit', (e) => {
-  e.preventDefault();
 
-  if (!fullName.value || !message.value) return null
-
-  var id = hiddenId.value || Date.now()
-
-  db.ref('reviews/' + id).set({
-    fullName: fullName.value,
-    message: message.value
-  });
-
-  fullName.value = '';
-  message.value  = '';
-  hiddenId.value = '';
-});
-
-// READ REVEIWS
+// TODO 6, Add listener on firebase event 'child_added'
 
 var reviews = document.getElementById('reviews');
-var reviewsRef = db.ref('/reviews');
 
-reviewsRef.on('child_added', (data) => {
-  var li = document.createElement('li')
-  li.id = data.key;
-  li.innerHTML = reviewTemplate(data.val())
-  reviews.appendChild(li);
-});
 
-reviewsRef.on('child_changed', (data) => {
-  var reviewNode = document.getElementById(data.key);
-  reviewNode.innerHTML = reviewTemplate(data.val());
-});
 
-reviewsRef.on('child_removed', (data) => {
-  var reviewNode = document.getElementById(data.key);
-  reviewNode.parentNode.removeChild(reviewNode);
-});
+// TODO 9, Add listener on firebase event 'child_changed'.
 
+
+  // TODO 12, Add listener on firebase event 'child_removed'.
+
+
+/**
+ * Update And Delete A Review
+ */
 reviews.addEventListener('click', (e) => {
   var reviewNode = e.target.parentNode
 
-  // UPDATE REVEIW
-  if (e.target.classList.contains('edit')) {
-    fullName.value = reviewNode.querySelector('.fullName').innerText;
-    message.value  = reviewNode.querySelector('.message').innerText;
-    hiddenId.value = reviewNode.id;
-  }
+  
+  // TODO 8, Add handler on button Edit
 
-  // DELETE REVEIW
-  if (e.target.classList.contains('delete')) {
-    var id = reviewNode.id;
-    db.ref('reviews/' + id).remove();
-  }
+
+  // TODO 11, Add handler on button Delete
+
 });
 
+
+/**
+ * Review Template
+ */
 function reviewTemplate({fullName, message}) {
   return `
     <div class='fullName'>${fullName}</div>
     <div class='message'>${message}</div>
-    <button class='delete'>Delete</button>
-    <button class='edit'>Edit</button>
+    // TODO 7, Add button Edit to template.
+
+    // TODO 10, Add button Delete to template.
   `
 };
